e92d45f1c36222c5c927c69d3fcbed927a3bea75
Jonathan Niez <116490400+JonathanNiez@users.noreply.github.com>
2023-09-08 14:00:31 +0800

    added id scanner model and tensorflow lite

--- app/src/main/java/com/capstone/idscanner/MainActivity.java
+++ app/src/main/java/com/capstone/idscanner/MainActivity.java
@@ -7,87 +7,94 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.net.ConnectivityManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.RequestManager;
 import com.capstone.idscanner.Utility.NetworkChangeReceiver;
 import com.capstone.idscanner.Utility.NetworkConnectivityChecker;
-import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.gms.tasks.OnFailureListener;
+import com.capstone.idscanner.databinding.ActivityMainBinding;
+import com.capstone.idscanner.ml.Model;
+import com.capstone.idscanner.ml.ModelUnquant;
 import com.google.firebase.storage.StorageReference;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import pl.droidsonroids.gif.GifImageView;
-
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton getImageBtn, calendarBtn;
-    private GifImageView loadingGif;
-    private ImageView idPreview;
     private Uri imageUri;
+    private Bitmap bitmap;
     private AlertDialog.Builder builder;
-    private AlertDialog optionsDialog, cancelScanIDDialog, noInternetDialog;
+    private AlertDialog optionsDialog, cancelScanIDDialog, noInternetDialog,
+            exitAppDialog;
     private TextRecognizer textRecognizer;
     private static final int CAMERA_REQUEST_CODE = 1;
     private static final int GALLERY_REQUEST_CODE = 2;
     private static final int CAMERA_PERMISSION_REQUEST = 101;
     private static final int STORAGE_PERMISSION_REQUEST = 102;
-    private String TAG = "DEBUG";
+    private final String TAG = "DEBUG";
+    private boolean shouldExit = false;
+    private Intent cameraIntent, galleryIntent;
     private NetworkChangeReceiver networkChangeReceiver;
     private StorageReference storageRef;
     private StorageReference imagesRef;
     private StorageReference fileRef;
     private RequestManager requestManager;
+    private ActivityMainBinding binding;
+    private BufferedReader bufferedReader;
+    private int count = 0;
+    private final int imageSize = 224;
+    private String[] labels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         initializeNetworkChecker();
         checkPermission();
+        initializeLabels();
 
-        getImageBtn = findViewById(R.id.getImageBtn);
-        idPreview = findViewById(R.id.idPreview);
-
-        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-
-        getImageBtn.setOnClickListener(v -> {
+        binding.getImageBtn.setOnClickListener(v -> {
 //            ImagePicker.with(this)
 //                    .crop()                    //Crop image(Optional), Check Customization for more option
 //                    .compress(1024)            //Final image size will be less than 1 MB(Optional)
 //                    .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
 //                    .start();
 //
-            showOptionsDialog();
+            showImageOptionsDialog();
+        });
+
+        binding.imgBackBtn.setOnClickListener(v -> {
+            showExitConfirmationDialog();
         });
 
     }
@@ -96,7 +103,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
+        closeImageOptionsDialog();
         closeNoInternetDialog();
+        closeExitConfirmationDialog();
     }
 
     @Override
@@ -107,7 +116,119 @@ public class MainActivity extends AppCompatActivity {
             unregisterReceiver(networkChangeReceiver);
         }
 
+        closeImageOptionsDialog();
         closeNoInternetDialog();
+        closeExitConfirmationDialog();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (shouldExit) {
+            super.onBackPressed(); // Exit the app
+        } else {
+            // Show an exit confirmation dialog
+            showExitConfirmationDialog();
+        }
+    }
+
+    private void exitApp() {
+        shouldExit = true;
+        onBackPressed();
+
+        finish();
+    }
+
+    private void initializeLabels() {
+        labels = new String[1000];
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("labels.txt")));
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                labels[count] = line;
+                count++;
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void classifyID(Bitmap bitmap) {
+        try {
+            ModelUnquant model = ModelUnquant.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+            int pixel = 0;
+            for (int n = 0; n < imageSize; n++) {
+                for (int i = 0; i < imageSize; i++) {
+                    int val = intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            ModelUnquant.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            float[] confidences = outputFeature0.getFloatArray();
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"Driver's License", "Senior Citizen ID", "PWD ID"};
+            binding.resultTextView.setText(classes[maxPos]);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            Model model = Model.newInstance(this);
+//
+//            // Creates inputs for reference.
+//            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
+//
+//            bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+//            inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
+//
+//            // Runs model inference and gets result.
+//            Model.Outputs outputs = model.process(inputFeature0);
+//
+//            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+//
+//            binding.resultTextView.setText(labels[getMax(outputFeature0.getFloatArray())]);
+//
+//            // Releases model resources if no longer used.
+//            model.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private int getMax(float[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[max]) max = 1;
+        }
+
+        return max;
     }
 
     private void checkPermission() {
@@ -130,13 +251,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openGallery() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         galleryIntent.setType("image/*");
         if (galleryIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
-            if (optionsDialog != null && optionsDialog.isShowing()) {
-                optionsDialog.dismiss();
-            }
+            closeImageOptionsDialog();
 
         } else {
             Toast.makeText(this, "No gallery app found", Toast.LENGTH_SHORT).show();
@@ -144,22 +263,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openCamera() {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
-            if (optionsDialog != null && optionsDialog.isShowing()) {
-                optionsDialog.dismiss();
-            }
+            closeImageOptionsDialog();
 
         } else {
             Toast.makeText(this, "No camera app found", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void showOptionsDialog() {
+    private void showImageOptionsDialog() {
         builder = new AlertDialog.Builder(this);
 
-        View dialogView = getLayoutInflater().inflate(R.layout.camera_gallery_dialog, null);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_camera_gallery, null);
 
         Button openCameraBtn = dialogView.findViewById(R.id.openCameraBtn);
         Button openGalleryBtn = dialogView.findViewById(R.id.openGalleryBtn);
@@ -174,9 +291,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         cancelBtn.setOnClickListener(v -> {
-            if (optionsDialog != null && optionsDialog.isShowing()) {
-                optionsDialog.dismiss();
-            }
+            closeImageOptionsDialog();
         });
 
         builder.setView(dialogView);
@@ -185,17 +300,10 @@ public class MainActivity extends AppCompatActivity {
         optionsDialog.show();
     }
 
-    private boolean matchesPattern(String text, String pattern) {
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(text);
-        return m.find();
-    }
-
-    private Uri getImageUri(Context context, Bitmap bitmap) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
-        return Uri.parse(path);
+    private void closeImageOptionsDialog() {
+        if (optionsDialog != null && optionsDialog.isShowing()) {
+            optionsDialog.dismiss();
+        }
     }
 
     @Override
@@ -207,11 +315,21 @@ public class MainActivity extends AppCompatActivity {
                 if (requestCode == CAMERA_REQUEST_CODE) {
                     if (data != null) {
 
-                        Bundle extras = data.getExtras();
-                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+//                        Bundle extras = data.getExtras();
+//                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+//                        imageUri = getImageUri(getApplicationContext(), bitmap);
 
-                        imageUri = getImageUri(getApplicationContext(), imageBitmap);
-                        idPreview.setImageURI(imageUri);
+                        binding.idPreview.setVisibility(View.VISIBLE);
+                        binding.textViewGuide.setVisibility(View.GONE);
+                        binding.resultTextView.setVisibility(View.VISIBLE);
+
+                        bitmap = (Bitmap) data.getExtras().get("data");
+                        int dimension = Math.min(bitmap.getWidth(), bitmap.getHeight());
+                        bitmap = ThumbnailUtils.extractThumbnail(bitmap, dimension, dimension);
+                        binding.idPreview.setImageBitmap(bitmap);
+
+                        bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false);
+                        classifyID(bitmap);
 
                         Toast.makeText(this, "Image is Loaded from Camera", Toast.LENGTH_LONG).show();
 
@@ -222,8 +340,17 @@ public class MainActivity extends AppCompatActivity {
                 } else if (requestCode == GALLERY_REQUEST_CODE) {
                     if (data != null) {
 
-                        imageUri = data.getData();
-                        idPreview.setImageURI(imageUri);
+                        binding.idPreview.setVisibility(View.VISIBLE);
+                        binding.textViewGuide.setVisibility(View.GONE);
+                        binding.resultTextView.setVisibility(View.VISIBLE);
+//                        imageUri = data.getData();
+//                        binding.idPreview.setImageURI(imageUri);
+
+                        Uri uri = data.getData();
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                        binding.idPreview.setImageBitmap(bitmap);
+
+                        classifyID(bitmap);
 
                         Toast.makeText(this, "Image is Loaded from Gallery", Toast.LENGTH_LONG).show();
 
@@ -233,8 +360,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             } else {
                 Toast.makeText(this, "Image is not Selected", Toast.LENGTH_SHORT).show();
-                Log.e(TAG + ":ERROR", "Failed to Extract Text");
-
                 Toast.makeText(this, "Failed to Extract Text", Toast.LENGTH_LONG).show();
             }
 
@@ -242,54 +367,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-    }
-
-    private void recognizeText() {
-
-        if (imageUri != null) {
-
-            try {
-                InputImage inputImage = InputImage.fromFilePath(MainActivity.this, imageUri);
-
-                textRecognizer.process(inputImage).addOnSuccessListener(text -> {
-
-                    String extractedText = text.getText();
-
-
-                    // Define patterns for PWD and Senior Citizen IDs
-                    String pwdPattern = "PWD-[0-9]{4}";
-                    String seniorCitizenPattern = "SeniorCitizen-[0-9]{4}";
-
-
-                    // Check if extracted text matches patterns
-//                    if (matchesPattern(extractedText, pwdPattern)) {
-//                        recognizedTextView.setText(extractedText);
-//                    } else if (matchesPattern(extractedText, seniorCitizenPattern)) {
-//                        recognizedTextView.setText(extractedText);
-//                    } else {
-//                        recognizedTextView.setText("Please use an ID");
-//                    }
-
-
-                }).addOnFailureListener(e -> {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                    e.printStackTrace();
-
-                });
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e(TAG + ":ERROR", "Failed to Extract Text");
-
-
-            }
-        } else {
-            Toast.makeText(this, "Image is not loaded", Toast.LENGTH_LONG).show();
-            Toast.makeText(this, "Failed to Extract Text", Toast.LENGTH_LONG).show();
-
-        }
     }
 
     @Override
@@ -326,6 +403,34 @@ public class MainActivity extends AppCompatActivity {
 
         noInternetDialog = builder.create();
         noInternetDialog.show();
+    }
+
+    private void showExitConfirmationDialog() {
+        builder = new AlertDialog.Builder(this);
+
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_exit_app, null);
+
+        Button yesBtn = dialogView.findViewById(R.id.yesBtn);
+        Button noBtn = dialogView.findViewById(R.id.noBtn);
+
+        yesBtn.setOnClickListener(v -> {
+            exitApp();
+        });
+
+        noBtn.setOnClickListener(v -> {
+            closeExitConfirmationDialog();
+        });
+
+        builder.setView(dialogView);
+
+        exitAppDialog = builder.create();
+        exitAppDialog.show();
+    }
+
+    private void closeExitConfirmationDialog() {
+        if (exitAppDialog != null && exitAppDialog.isShowing()) {
+            exitAppDialog.dismiss();
+        }
     }
 
     private void closeNoInternetDialog() {
